816c2e91dbcac2a57f864160bfb1f79f6b153cb0
Agustindardanelli-stack <agusdardanelli16@gmail.com>
2023-05-25 15:32:12 -0300

    Fixed some conflict

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -39,8 +39,8 @@ export default function Footer ()  {
                     </li>
                 </ul>
             </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500  sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">L´Antonia Sin Tacc</a>. All Rights Reserved.</span>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-gray-500  sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">L´Antonia Sin Tacc</a>. All Rights Reserved.</span>
         </div>
         </footer>
 
