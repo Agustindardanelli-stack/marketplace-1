32bc6795d656543dfc93f4d8b1a73a368998d94f
Agustindardanelli-stack <agusdardanelli16@gmail.com>
2023-07-27 21:23:29 -0300

    remove

--- backend/config/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-//midelwers
-app.use(express.json)
-app.use(express.urlencoded({extended: true}))
-
-
-module.exports = app 
--- backend/controllers/index.js
+++
@@ -1,2 +0,0 @@
-const addProductos = require('./addProductos')
-
